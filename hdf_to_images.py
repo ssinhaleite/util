8ca1256f258de2119084a6f72adcdb2496b3a37a
Vanessa Leite <vanessa@ini.uzh.ch>
2018-10-03 10:20:55 +0200

    Update the script to receive parameters on command line
    not tested :P

--- hdf_to_images.py
+++ hdf_to_images.py
@@ -1,19 +1,42 @@
 import numpy as np
-from PIL import Image
 import h5py
+import sys
+import os
 
-if __name__ == "__main__":
+from PIL import Image
 
-    lm_path = "/media/vleite/a5c2cdb0-d068-41ea-a7a1-b28a29d082f3/ThomasDataset/Wafer1/C1_EM_2sections.hdf"
+# generates images from an hdf5 file.
+# input path to the hdf file, dataset_path, path to save the images
+# The images are generated considering that the hdf has 3-dimensions (z, y, x). Will be generated z images of (y,x).
 
-    hdf_file = h5py.File(lm_path, "r")
-    imgs = hdf_file["volumes/raw"]
+def generate_images(data_path, dataset, output_path):
+    hdf_file = h5py.File(data_path, "r")
+    imgs = hdf_file[dataset]
     img_array = np.asarray(imgs).astype("uint8")
+    
     print(img_array.shape)
     print(img_array.dtype)
 
     for i in range(imgs.shape[0]):
         print("Saving image {}".format(i))
         im = Image.fromarray(img_array[i, :])
-        im.save("/home/vleite/testes" + "/" + str(i).zfill(5) + ".tif")
-        exit()
+        im.save(output_path + str(i).zfill(5) + ".tif")
+
+if __name__ == "__main__":
+    
+    basic_dir = "/media/vleite/a5c2cdb0-d068-41ea-a7a1-b28a29d082f3/unet-songbird/01_data/soma/"
+    data_path = basic_dir + "affinities/segmentation_soma_13120-16000_7872-9664_320-514_120000_0.19.hdf"
+    dataset = "volumes/raw"
+    output_path = basic_dir + "imgs/segmentation_soma_13120-16000_7872-9664_320-514_120000_0.19/"
+    
+    # first argv is the script name
+    if len(sys.argv) == 4:
+        data_path = sys.argv[1]
+        dataset = sys.argv[2]
+        output_path = sys.argv[3]
+
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
+
+    generate_images(data_path, dataset, output_path)
+
